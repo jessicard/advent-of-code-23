4f55fa20cb6ea7046e825794ffc3d50e2caf2d92
Jessica Card <jc6221@columbia.edu>
2023-12-02 15:15:40 -0500

    Working but wrong answer

--- jessica/day01/part2/part2.c
+++ jessica/day01/part2/part2.c
@@ -19,12 +19,103 @@ int main(int argc, char **argv)
     }
 
     char buf[1000] = { 0 };
+    char ln[1000] = { 0 };
     int tot = 0;
 
     while (fgets(buf, sizeof(buf), fp) != NULL)
     {
-        fprintf(stderr, "%s\n", buf);
+        char num[1000] = { '\0' };
+        memset(ln, 0, sizeof(ln));
+
+        for (int i = 0; i < strlen(buf); i++)
+        {
+            // fprintf(stderr, "\n%lu\n", strlen(ln));
+            ln[strlen(ln)] = buf[i];
+
+            if (strstr(ln, "one") != NULL)
+            {
+                sprintf(&num[strlen(num)], "%c", '1');
+                //fprintf(stderr, "%s", "\none\n");
+                memset(ln, 0, sizeof(ln));
+                continue;
+            }
+            if (strstr(ln, "two") != NULL)
+            {
+                sprintf(&num[strlen(num)], "%c", '2');
+                // fprintf(stderr, "%s", "\ntwo\n");
+                memset(ln, 0, sizeof(ln));
+                continue;
+            }
+            if (strstr(ln, "three") != NULL)
+            {
+                sprintf(&num[strlen(num)], "%c", '3');
+                // fprintf(stderr, "%s", "\nthree\n");
+                memset(ln, 0, sizeof(ln));
+                continue;
+            }
+            if (strstr(ln, "four") != NULL)
+            {
+                sprintf(&num[strlen(num)], "%c", '4');
+                // fprintf(stderr, "%s", "\nfour\n");
+                memset(ln, 0, sizeof(ln));
+                continue;
+            }
+            if (strstr(ln, "five") != NULL)
+            {
+                sprintf(&num[strlen(num)], "%c", '5');
+                // fprintf(stderr, "%s", "\nfive\n");
+                memset(ln, 0, sizeof(ln));
+                continue;
+            }
+            if (strstr(ln, "six") != NULL)
+            {
+                sprintf(&num[strlen(num)], "%c", '6');
+                // fprintf(stderr, "%s", "\nsix\n");
+                memset(ln, 0, sizeof(ln));
+                continue;
+            }
+            if (strstr(ln, "seven") != NULL)
+            {
+                sprintf(&num[strlen(num)], "%c", '7');
+                // fprintf(stderr, "%s", "\nseven\n");
+                memset(ln, 0, sizeof(ln));
+                continue;
+            }
+            if (strstr(ln, "eight") != NULL)
+            {
+                sprintf(&num[strlen(num)], "%c", '8');
+                // fprintf(stderr, "%s", "\neight\n");
+                memset(ln, 0, sizeof(ln));
+                continue;
+            }
+            if (strstr(ln, "nine") != NULL)
+            {
+                sprintf(&num[strlen(num)], "%c", '9');
+                // fprintf(stderr, "%s", "\nnine\n");
+                memset(ln, 0, sizeof(ln));
+                continue;
+            }
+
+            // fprintf(stderr, "%c", ln[i]);
+
+            if (isdigit(buf[i]))
+            {
+                // fprintf(stderr, "%c", buf[i]);
+                sprintf(&num[strlen(num)], "%c", buf[i]);
+                memset(ln, 0, sizeof(ln));
+            }
+
+        }
+
+        num[1] = num[strlen(num) - 1];
+        num[2] = '\0';
+
+        fprintf(stderr, "%d\n", j);
+
+        tot += atoi(num);
     }
+
+    fprintf(stderr, "%d", tot);
 
     return 0;
 }
