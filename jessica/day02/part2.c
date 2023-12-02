5fb24477124dbc6f403aa478bb1bcbccc0b22e5b
Jessica Card <jc6221@columbia.edu>
2023-12-02 14:19:11 -0500

    Cleanup

---
+++ jessica/day02/part2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, char **argv)
+{
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <input_file>\n", argv[0]);
+        exit(1);
+    }
+
+    char *filename = argv[1];
+    FILE *fp = fopen(filename, "rb");
+
+    if (fp == NULL) {
+        perror(filename);
+        exit(1);
+    }
+
+    char buf[1000] = { 0 };
+    int tot = 0;
+
+    while (fgets(buf, sizeof(buf), fp) != NULL)
+    {
+        fprintf(stderr)
+    }
+
+    return 0;
+}
