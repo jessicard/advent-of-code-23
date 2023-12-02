63b6c86b22785e5e2ab922ffe95903b8b5d0f4dc
Jake Card <jacob.boxer@gmail.com>
2023-12-02 13:43:38 -0500

    Add file reading

--- jake/day01/part1.c
+++ jake/day01/part1.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char **argv) {
-    for (int i = 0; i < argc; i++) {
-        printf("%s\n", argv[i]);
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
     }
 
+    // Open the file
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL) {
+        fprintf(stderr, "Error opening file\n");
+        return 1;
+    }
+
+    // Read and print the contents of the file
+    char ch;
+    while ((ch = fgetc(file)) != EOF) {
+        putchar(ch);
+    }
+
+    fclose(file);
+    putchar('\n');
     return 0;
 }
