805310a0a22bfc60b882ea85035748000da9b095
Jessica Card <jc6221@columbia.edu>
2023-12-03 13:16:34 -0500

    Part 2

--- jessica/day02/part2/part2.rb
+++ jessica/day02/part2/part2.rb
@@ -12,35 +12,28 @@ File.foreach("input").with_index do |line, i|
   games[i + 1]["blue"] = 0
   games[i + 1]["green"] = 0
 
-  # Set up test case
-  test = {}
-  test["red"] = 12
-  test["green"] = 13
-  test["blue"] = 14
-
   rds.each do |rd|
     rd = rd.strip.split(", ")
     rd.each do |grab|
       color = grab.tr('^A-Za-z', '')
       amt = Integer(grab.tr('^0-9', ''))
-      games[i + 1][color] += amt
-    end
-  end
-
-  games.each do |key, arr|
-    arr.each do |color, val|
-      if arr[color] > test[color]
-        puts "Game #{key} fails from #{color} being #{arr[color]}"
-        games.delete(i + 1)
-        break
+      if amt > games[i + 1][color]
+        games[i + 1][color] = amt
       end
     end
   end
 end
 
-games.each do |key, arr|
-  puts key, arr
-  tot += key
+games.each do |key, val|
+  pwr = 0
+  val.each do |color, amt|
+    if pwr == 0
+      pwr = amt
+    else
+      pwr *= amt
+    end
+  end
+  tot += pwr
 end
 
 puts tot
